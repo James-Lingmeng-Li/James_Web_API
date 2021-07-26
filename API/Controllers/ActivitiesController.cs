60f0ba91ff43ac8222df1b9508378913635c729d
James Li <lilingmeng@gmail.com>
2021-07-26 21:30:47 +1000

    Edit activity added

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,5 +19,16 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query { Id = id });
         }
+        [HttpPost]
+        public async Task<ActionResult<Activity>> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Activity>> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
     }
 }
